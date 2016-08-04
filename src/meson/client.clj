270d7273bdff5f6262b9e4fd2bdd829b2b330f30
Duncan McGreggor <oubiwann@gmail.com>
2016-08-03 19:35:24 -0500

    Some minor refactoring.

--- src/meson/client.clj
+++ src/meson/client.clj
@@ -12,13 +12,17 @@
      :log-level :info}})
 
 (defprotocol BaseClientAPI
-  (get-context [self]
+  (get-context [this]
     "Get the context for this client, calculated using `:base-path` and
     `:version`."))
 
-(defrecord BaseClient [] BaseClientAPI
-  (get-context [self]
-    (format "%s/v%s" (:base-path self) (:version self))))
+(defrecord BaseClient [])
+
+(def client-behaviour
+  {:get-context (fn [this]
+    (format "%s/v%s" (:base-path this) (:version this)))})
+
+(extend BaseClient BaseClientAPI client-behaviour)
 
 (defn ->base-client
   "Unlike `->BaseClient`, this factory function takes a map of the client
@@ -26,6 +30,8 @@
   `map->BaseClient`. An important difference is that if no argument is given,
   the default base client map, `base-client-fields`, is used."
   ([]
-    (->base-client base-client-fields))
+    (->base-client {}))
   ([fields]
-    (map->BaseClient fields)))
+    (->> fields
+         (into base-client-fields)
+         (map->BaseClient))))
