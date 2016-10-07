3526be0b4f141ec9be742905a1a9f248dd30378f
Robert Magneson <hromagnum@users.noreply.github.com>
2016-10-07 17:39:53 -0500

    Update start_mesos.sh

--- test/start_mesos.sh
+++ test/start_mesos.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
-sudo nohup /usr/sbin/mesos-master.sh --work_dir=/tmp/mesos-master --ip=127.0.0.1 > master.log &
-sudo nohup /usr/sbin/mesos-agent.sh --work_dir=/tmp/mesos-agent --master=127.0.0.1:5050 > agent.log &
+sudo nohup /usr/sbin/mesos-master --work_dir=/tmp/mesos-master --ip=127.0.0.1 > master.log &
+sudo nohup /usr/sbin/mesos-agent --work_dir=/tmp/mesos-agent --master=127.0.0.1:5050 > agent.log &
+
+sleep 10
+
+echo
+echo "*** MASTER LOG ***"
+echo
+cat master.log
+echo
+echo "*** AGENT LOG ***"
+echo
+cat agent.log
+echo
+echo
