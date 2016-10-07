6085745e64d2fcb4825b6e0afb96413c73cdaa59
Robert Magneson <hromagnum@users.noreply.github.com>
2016-10-07 17:15:30 -0500

    Add sudo.

--- test/install_deps.sh
+++ test/install_deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 curl --remote-name http://repos.mesosphere.com/ubuntu/pool/main/m/mesos/mesos_1.0.0-2.0.89.ubuntu1404_amd64.deb
-dpkg -i mesos_1.0.0-2.0.89.ubuntu1404_amd64.deb
+sudo dpkg -i mesos_1.0.0-2.0.89.ubuntu1404_amd64.deb
 rm mesos_1.0.0-2.0.89.ubuntu1404_amd64.deb
 ls -al /etc/init.d/m*
