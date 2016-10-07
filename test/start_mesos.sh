bb3b5febb4aadaf27b87dec954e2c0860a1e6036
Robert Magneson <hromagnum@users.noreply.github.com>
2016-10-07 17:37:54 -0500

    Get Mesos started on Travis.

--- test/start_mesos.sh
+++ test/start_mesos.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-nohup ./bin/mesos-master.sh --work_dir=/tmp/mesos-master --ip=10.0.4.193 > master.log &
-sudo nohup ./bin/mesos-slave.sh --work_dir=/tmp/mesos-agent --master=10.0.4.193:5050 > agent.log &
+sudo nohup /usr/sbin/mesos-master.sh --work_dir=/tmp/mesos-master --ip=127.0.0.1 > master.log &
+sudo nohup /usr/sbin/mesos-agent.sh --work_dir=/tmp/mesos-agent --master=127.0.0.1:5050 > agent.log &
