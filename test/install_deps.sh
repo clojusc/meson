d4bf4fc415a630c9b3aad0f889572e5743fa4995
Robert Magneson <hromagnum@users.noreply.github.com>
2016-10-07 17:26:47 -0500

    Missing Mesos dep.

--- test/install_deps.sh
+++ test/install_deps.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo apt-get install libevent-dev
+
 curl --remote-name http://repos.mesosphere.com/ubuntu/pool/main/m/mesos/mesos_1.0.0-2.0.89.ubuntu1404_amd64.deb
 sudo dpkg -i mesos_1.0.0-2.0.89.ubuntu1404_amd64.deb
 rm mesos_1.0.0-2.0.89.ubuntu1404_amd64.deb
