3ad76b07fde23d0b9eac8247b98958bf50a6fd53
Robert Magneson <hromagnum@users.noreply.github.com>
2016-10-07 17:32:15 -0500

    Upgrade to Mesos 1.0.1.
    Also, use some vars.

--- test/install_deps.sh
+++ test/install_deps.sh
@@ -2,7 +2,11 @@
 
 sudo apt-get install libevent-dev
 
-curl --remote-name http://repos.mesosphere.com/ubuntu/pool/main/m/mesos/mesos_1.0.0-2.0.89.ubuntu1404_amd64.deb
-sudo dpkg -i mesos_1.0.0-2.0.89.ubuntu1404_amd64.deb
-rm mesos_1.0.0-2.0.89.ubuntu1404_amd64.deb
+MESOS_DEB=mesos_1.0.1-2.0.93.ubuntu1404_amd64.deb
+MESOS_DL=http://repos.mesosphere.com/ubuntu/pool/main/m/mesos/$MESOS_DEB
+curl --remote-name $MESOS_DL
+sudo dpkg -i $MESOS_DEB
+rm $MESOS_DEB
 ls -al /etc/init.d/m*
+which mesos-master
+which mesos-agent
