36b903a7abd84cd51e9f1a22293405c878499ca8
Robert Magneson <hromagnum@users.noreply.github.com>
2016-10-08 15:16:50 -0500

    Removed placeholder, no-op tests.

--- test/meson/core_test.clj
+++ test/meson/core_test.clj
@@ -1,7 +1,3 @@
 (ns meson.core-test
   (:require [clojure.test :refer :all]
             [meson.core :as meson]))
-
-(deftest a-test
-  (testing "I don't fail, but I don't do much."
-    (is (= 1 1))))
--- test/meson/http_test.clj
+++ test/meson/http_test.clj
@@ -2,6 +2,3 @@
   (:require [clojure.test :refer :all]
             [meson.http :as http]))
 
-(deftest get-verb
-  (testing "I don't fail, but I don't do much."
-    (is (= 1 1))))
