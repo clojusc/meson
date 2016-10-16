03359a9e3477ab17aef8014922bda6d81cd6a762
Duncan McGreggor <oubiwann@gmail.com>
2016-10-16 13:41:23 -0500

    Added tests for recordio.

---
+++ test/meson/testing/payloads.clj
@@ -0,0 +1,27 @@
+(ns meson.testing.payloads
+  (:require [meson.util :as util]))
+
+(defn subscribe
+  []
+  (-> (str "139\n"
+           "{\"type\":\"SUBSCRIBE\","
+           "\"subscribe\":{\"framework_info\":{"
+                               "\"user\":\"Alice\","
+                               "\"name\":\"Test Clojure\\/HTTP Framework (Clojure)\","
+                               "\"hostname\":\"127.0.0.1\"}}}")
+      (util/str->stream)))
+
+(defn subscribed
+  []
+  (-> (str "139\n"
+           "{\"type\":\"SUBSCRIBED\","
+           "\"subscribed\":{\"framework_id\":{"
+                                "\"value\":\"5f1bf690-0145-44be-96ae-145e5c51fccc-0017\"},"
+                           "\"heartbeat_interval_seconds\":15.0}}")
+      (util/str->stream)))
+
+(defn heartbeat
+  []
+  (-> (str "20\n"
+           "{\"type\":\"HEARTBEAT\"}")
+      (util/str->stream)))
---
+++ test/meson/util/recordio_test.clj
@@ -0,0 +1,60 @@
+(ns meson.util.recordio-test
+  (:require [clojure.test :refer :all]
+            [meson.client.master :as master]
+            [meson.testing.payloads :as payload]
+            [meson.util :as util]
+            [meson.util.recordio :as recordio]))
+
+(def testing-host "127.0.0.1")
+(def testing-master {:master (format "%s:5050" testing-host)})
+(def framework-info
+  {:framework_info
+    {:user "Alice"
+     :name "Test Clojure/HTTP Framework (Clojure)"
+     :hostname testing-host}})
+
+;;; Unit Tests ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest ^:unit get-size
+  (is (= (recordio/get-size! (payload/subscribed)) 139)))
+
+(deftest ^:unit get-data
+  (let [stream (payload/subscribed)
+        record-size (recordio/get-size! stream)
+        bytes (recordio/get-data! stream record-size)
+        json (util/bytes->json bytes)]
+    (is (= (:type json) :subscribed))
+    (is (= (get-in json [:subscribed :framework_id :value])
+           "5f1bf690-0145-44be-96ae-145e5c51fccc-0017"))))
+
+(deftest ^:unit next
+  (let [bytes (recordio/next! (payload/subscribed))
+        json (util/bytes->json bytes)]
+    (is (= (:type json) :subscribed))
+    (is (= (get-in json [:subscribed :framework_id :value])
+           "5f1bf690-0145-44be-96ae-145e5c51fccc-0017"))))
+
+;;; Integration Tests ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest ^:integration recordio-methods
+  (testing "get-size! and get-data! ..."
+    (let [c (master/create testing-master)
+          response (master/subscribe c framework-info)
+          stream (:body response)
+          record-size (recordio/get-size! stream)
+          bytes (recordio/get-data! stream record-size)
+          json (util/bytes->json bytes)]
+      (is (= record-size 139))
+      (is (= (:type json) :subscribed))
+      (is (= (get-in json [:subscribed :heartbeat_interval_seconds]) 15.0))))
+  (testing "just next! ..."
+    (let [c (master/create testing-master)
+          response (master/subscribe c framework-info)
+          bytes (recordio/next! (:body response))
+          json (util/bytes->json bytes)]
+      (is (= (:type json) :subscribed))
+      (is (= (get-in json [:subscribed :heartbeat_interval_seconds]) 15.0)))))
+
+;;; System Tests ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; TBD
